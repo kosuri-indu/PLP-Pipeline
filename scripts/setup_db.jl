b1b36db59194c30f4008a13176ca548bedcfecc6
Kosuri Indu <kosurilindu@gmail.com>
2025-03-02 12:14:36 +0530

    added NULL

--- scripts/setup_db.jl
+++ scripts/setup_db.jl
@@ -56,7 +56,7 @@ for file in readdir(raw_data_dir, join=true)
         df = load_parquet_to_df(file)
         DBInterface.execute(connection, "DROP TABLE IF EXISTS $table_name")
 
-        column_definitions = join(["$column_name $(get_sql_type(eltype(df[!, column_name])))" for column_name in names(df)], ", ")
+        column_definitions = join(["$column_name $(get_sql_type(eltype(df[!, column_name])))" for column_name in names(df)],", ")
         DBInterface.execute(connection, "CREATE TABLE $table_name ($column_definitions)")
 
         # Load data in batches - useful for large datasets
@@ -65,10 +65,9 @@ for file in readdir(raw_data_dir, join=true)
             batch = df[i:min(i + batch_size - 1, nrow(df)), :]
             values_list = []
             for row in eachrow(batch)
-                values = join(["'$(replace_single_quotes(string(row[col])))'" for col in names(df)], ", ")
+                values = join([ismissing(row[col]) ? "NULL" : "'$(replace_single_quotes(string(row[col])))'" for col in names(df)],", ")
                 push!(values_list, "($values)")
             end
-
             if !isempty(values_list)
                 insert_query = "INSERT INTO $table_name VALUES " * join(values_list, ", ")
                 DBInterface.execute(connection, insert_query)
@@ -76,6 +75,9 @@ for file in readdir(raw_data_dir, join=true)
         end
 
         result = DBInterface.execute(connection, "SELECT COUNT(*) FROM $table_name")
+        row_count = DataFrame(result)[1, 1]
+        col_count = length(names(df))
+        println("Table $table_name loaded with $row_count rows and $col_count columns.")
     end
 end
 
