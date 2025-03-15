e2a5b84fbaad1ce794c26ddc842a6ddee948253c
Kosuri Indu <kosurilindu@gmail.com>
2025-03-15 20:14:14 +0530

    checked data from new DuckDB

--- src/data_loader.jl
+++ src/data_loader.jl
@@ -5,17 +5,27 @@ using DBInterface
 using DuckDB
 using PrettyTables
 
-connection = DBInterface.connect(DuckDB.DB, datadir("omop.duckdb"))
+connection = DBInterface.connect(DuckDB.DB, datadir("exp_raw", "synthea_1M_3YR.duckdb"))
+
+const SCHEMA = "dbt_synthea_dev"
 
 function test_data()
-    # few example tables for querying
-    tables = [
-        "concept", "concept_relationship", "condition_occurrence",
-    ]
+    tables_query = "SELECT table_name FROM information_schema.tables WHERE table_schema = '$SCHEMA'"
+    tables_result = DBInterface.execute(connection, tables_query)
+    tables = [row[1] for row in tables_result]
+
+    println("Tables in database under schema '$SCHEMA':")
+    println(tables)
 
     for table in tables
+
+        # skipping staging tables
+        if startswith(table, "stg_")
+            continue
+        end
+
         println("Checking: $table")
-        query = "SELECT * FROM $table LIMIT 5"
+        query = "SELECT * FROM $SCHEMA.$table LIMIT 5"
         result = DBInterface.execute(connection, query)
         pretty_table(result)
         println("\n")
@@ -23,3 +33,5 @@ function test_data()
 end
 
 test_data()
+
+DBInterface.close!(connection)
